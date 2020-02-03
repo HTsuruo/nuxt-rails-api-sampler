3524bdaf8f203fccddd3df80c1eba6ce7a2cc635
HTsuruo <tsuru.dev@gmail.com>
2020-02-04 08:26:30 +0900

    add: ページ遷移を追加.

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
-    <top-cover title="Welcome." description="ようこそnuxt-rails-samplerへ。" />
+    <top-cover title="Index." description="これはインデックスページです。リストが表示されます。" />
+    <div class="container">
+      <table class="table is-striped is-fullwidth">
+        <thead>
+          <th>ID</th>
+          <th>タイトル</th>
+          <th>説明</th>
+          <th></th>
+        </thead>
+        <tbody>
+          <tr v-for="post in posts" :key="post.id">
+            <td>{{ post.id }}</td>
+            <td>{{ post.title }}</td>
+            <td>{{ post.description }}</td>
+            <td><nuxt-link :to="`/posts/${post.id}`" class="button is-link is-light is-small">詳細へ</nuxt-link></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -10,6 +28,14 @@ import TopCover from '~/components/TopCover.vue'
 export default {
   components: {
     TopCover
+  },
+  async asyncData ({ $axios }) {
+    const res = await $axios.get('http://localhost:3000/posts')
+    // .catch((error) => {
+    //   console.log(error)
+    //   return false
+    // })
+    return { posts: res.data }
   }
 }
 </script>
--- frontend/pages/posts/_id.vue
+++ frontend/pages/posts/_id.vue
@@ -2,8 +2,31 @@
   <div>
     <top-cover title="Detail." description="選択されたコンテンツの詳細が表示されます" />
     <div class="container">
-      ああ
-      {{ post }}
+      <div class="columns">
+        <div class="column is-half">
+          <table class="table is-fullwidth">
+              <tbody>
+                <tr>
+                  <td>ID</td>
+                  <td>{{ post.id }}</td>
+                </tr>
+                <tr>
+                  <td>タイトル</td>
+                  <td>{{ post.title }}</td>
+                </tr>
+                <tr>
+                  <td>説明</td>
+                  <td>{{ post.description }}</td>
+                </tr>
+                <tr>
+                  <td>作成日</td>
+                  <td>{{ post.created_at }}</td>
+                </tr>
+              </tbody>
+          </table>
+          <nuxt-link to="/" class="button is-link is-light">一覧画面へ</nuxt-link>
+        </div>
+      </div>
     </div>
   </div>
 </template>
